d2fa19becb5f8cc47b1627598745689baec2c064
underrooz <ParmarTarun>
2023-02-10 19:31:04 -0500

    :sparkles: Basic Project Page

--- ui/src/pages/Projects.tsx
+++ ui/src/pages/Projects.tsx
@@ -1,5 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+import ProjectCard from "../components/ProjectCard";
+
 const Projects = () => {
-    return <div>Projects</div>;
+    return (
+        <Container className="projects-page">
+            <h3>Projects</h3>
+            <Container className="project-cards-wrapper">
+                <Row xs={1} md={3}>
+                    {[1, 2, 3, 4, 5].map((i) => (
+                        <Col>
+                            <ProjectCard key={i} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </Container>
+    );
 };
 
 export default Projects;
