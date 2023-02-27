2a5f6932ea611311bc98d95d8be8ccb435790c4f
Rishank Karkera <rishankkarkera96@gmail.com>
2023-02-26 19:53:46 -0500

    Setup boilerplate logic for controllers, service and dto and configured gradle (#4)
    * Setup boilerplate code for User related components like service, controller, DTO
    
    * Adding gradle & misc configurations

---
+++ research-survey-extractor-api/src/main/java/com/bing/researchsurveyextractorapi/controllers/UserController.java
@@ -0,0 +1,64 @@
+package com.bing.researchsurveyextractorapi.controllers;
+
+import com.bing.researchsurveyextractorapi.dto.UserDTO;
+import com.bing.researchsurveyextractorapi.models.User;
+import com.bing.researchsurveyextractorapi.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/${API_VERSION_V1}/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/authenticate")
+    public UserDTO authenticateUser(@RequestParam String username, @RequestParam String password) {
+        return userService.authenticateUser(username, password);
+    }
+
+    @GetMapping("")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("{username}")
+    public UserDTO getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    @GetMapping("email/{email}")
+    public UserDTO getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
+
+    @PostMapping("")
+    public void registerUser(User user) {
+        userService.createUser(user);
+    }
+
+    @PutMapping("")
+    public void updateUserDetails(User user) {
+        userService.updateUserDetails(user);
+    }
+
+    @PostMapping("/changePassword")
+    public void changePassword(String oldPassword, String newPassword) {
+        userService.updatePassword(null, oldPassword, newPassword);
+    }
+
+    @GetMapping("/exists/{username}")
+    public boolean checkUserExistsByUsername(@PathVariable String username) {
+        return userService.checkUserExistsByUsername(username);
+    }
+
+    @GetMapping("/exists/email/{email}")
+    public boolean checkUserExistsByEmail(@PathVariable String email) {
+        return userService.checkUserExistsByEmail(email);
+    }
+}
