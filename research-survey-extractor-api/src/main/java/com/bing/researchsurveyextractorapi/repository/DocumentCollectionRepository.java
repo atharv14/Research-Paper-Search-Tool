65edcea182713d8a26af0ff2c7220cba0c73dfa5
Rishank Karkera <rishankkarkera96@gmail.com>
2023-03-25 15:55:54 -0400

    Feature/models (#17)
    * Completed implementing API functionalities for User, Project, Query, SearchResults & DocumentCollection
    
    * Fixed sonar code quality issues
    
    * Fixed sonar code quality issues

--- research-survey-extractor-api/src/main/java/com/bing/researchsurveyextractorapi/repository/DocumentCollectionRepository.java
+++ research-survey-extractor-api/src/main/java/com/bing/researchsurveyextractorapi/repository/DocumentCollectionRepository.java
@@ -2,18 +2,9 @@ package com.bing.researchsurveyextractorapi.repository;
 
 import com.bing.researchsurveyextractorapi.models.DocumentCollection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
 public interface DocumentCollectionRepository extends JpaRepository<DocumentCollection, Long> {
-    @Transactional
-    @Modifying
-    @Query("update DocumentCollection d set d.collectionName = ?1 where d.collectionId = ?2")
-    int updateCollectionNameByCollectionId(String collectionName, long collectionId);
-
     Collection<DocumentCollection> findByProjectProjectId(long projectId);
-
 }
