2c9111576144a9eb76e43558464c56b5c2478fdc
underrooz <ParmarTarun>
2023-04-04 11:24:48 -0400

    :sparkles: Add Create New Project Modal

--- ui/src/pages/Dashboard.tsx
+++ ui/src/pages/Dashboard.tsx
@@ -1,12 +1,20 @@
-import { Col, Container, Row, Spinner } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Col, Container, Row, Spinner } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import ProjectCard from "../components/ProjectCard";
 import { getProjects } from "../api/project";
 import { useEffect, useState } from "react";
 import { projectType } from "../api/types";
+import { isUserLoggedIn } from "../api/utility";
+import NewPorjectModal from "../components/NewPorjectModal";
 const Dashbaord = () => {
     const [projects, setProjects] = useState<projectType[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [showNewProjectModal, setShowNewProjectModal] = useState(false);
+    const navigate = useNavigate();
+
+    const saveProject = (name: string, description: string) => {
+        console.log(name, description);
+    };
 
     const fetchProjects = () => {
         getProjects()
@@ -18,11 +26,31 @@ const Dashbaord = () => {
             .finally(() => setIsLoading(false));
     };
 
-    useEffect(fetchProjects, []);
+    useEffect(() => {
+        if (!isUserLoggedIn()) {
+            navigate("/auth");
+            return;
+        }
+        fetchProjects();
+    }, []);
 
     return (
         <Container className="dashboard-page">
-            <h3>Projects</h3>
+            <Container className="p-4">
+                <Row className="justify-content-between">
+                    <Col md={2}>
+                        <h3>Projects</h3>
+                    </Col>
+                    <Col md={2}>
+                        <Button
+                            variant="success"
+                            onClick={() => setShowNewProjectModal(true)}
+                        >
+                            Create New
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
             <Container className="project-cards-wrapper">
                 {!isLoading ? (
                     <Row xs={1} md={3}>
@@ -46,6 +74,11 @@ const Dashbaord = () => {
                     <Spinner />
                 )}
             </Container>
+            <NewPorjectModal
+                show={showNewProjectModal}
+                handleClose={() => setShowNewProjectModal(false)}
+                saveProject={saveProject}
+            />
         </Container>
     );
 };
